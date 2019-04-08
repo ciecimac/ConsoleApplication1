6b6c14be0735f3809e5802b3f78cd69c3bfd93b7
ciecclm <ciecclm@imnu.edu.cn>
2019-04-08 15:38:44 +0800

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,117 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+class CMyDate
+{
+private:
+	int year;
+	int month;
+	int day;
+	int check();
+public:
+	CMyDate();
+	CMyDate(int y, int m, int d);
+	int setDate(int y, int m, int d);
+	void displayDate();
+	void addOneDay();
+};
+CMyDate::CMyDate()
+{
+	year = 2019;
+	month = 1;
+	day = 1;
+}
+CMyDate::CMyDate(int y, int m, int d)
+{
+	year = y;
+	month = m;
+	day = d;
+	if (check() == 1 || check() == -1)
+	{
+		month = 1;
+		day = 1;
+		cout <<"设置了错误日期，月日已置1"<< endl;
+    }
+	
+}
+int CMyDate::setDate(int y, int m, int d)
+{
+	year = y;
+	month = m;
+	day = d;
+	if (check() == 1 || check() == -1)
+	{
+		month = 1;
+		day = 1;
+		cout << "设置了错误日期，月日已置1" << endl;
+		return -1;
+	}
+	return 0;
+
+}
+void CMyDate::displayDate()
+{
+	cout << year << "年" << month << "月" << day << "日" << endl;
+}
+int CMyDate::check()
+{
+	int flatYearDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int leapYearDays[12] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+    if (month > 12 || month < 1)
+		return -1;
+	if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+	{
+		if (leapYearDays[month - 1] < day || day < 1)
+			return 1;
+	}
+	else
+	{
+		if (flatYearDays[month - 1] < day || day < 1)
+			return 1;
+	}
+	return 0;
+}
+void CMyDate::addOneDay()
+{
+	int flatYearDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int leapYearDays[12] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+	day++;
+	
+	if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+	{
+		if (leapYearDays[month - 1] < day)
+		{
+			day = 1;
+			month++;
+		}
+	}
+	else
+	{
+		if (flatYearDays[month - 1] < day)
+		{
+			day = 1;
+			month++;
+		}
+	}
+	if (month > 12)
+	{
+		month = 1;
+		year++;
+    }
+	
+}
+int main()
+{
+	CMyDate od;
+	int i;
+	od.setDate(2016, 4, 8);
+	od.displayDate();
+	for(i=0;i<100;i++)
+		od.addOneDay();
+	od.displayDate();
+    return 0;
+}
+
